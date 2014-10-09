7799e4daec0dbe6b70befa60227595da9a932723
Walker Henderson <wjahenderson@gmail.com>
2014-10-09 12:17:03 -0500

    Added db connect to the route

--- src/router.py
+++ src/router.py
@@ -2,10 +2,6 @@ from flask import *
 from random import randint
 import MySQLdb
 
-db = MySQLdb.connect(host="localhost",
-                     user="root",
-                     passwd="",
-                     db="test")
 
 app = Flask(__name__,
             static_folder="../static",
@@ -14,9 +10,14 @@ app = Flask(__name__,
 
 @app.route('/')
 def root():
+    db = MySQLdb.connect(host="localhost",
+                         user="root",
+                         passwd="",
+                         db="test")
     cur = db.cursor()
     cur.execute("SELECT * FROM sayings")
     sayings = list(cur.fetchall())
+    db.close()
     return sayings[randint(0, len(sayings) - 1)]
 
 
