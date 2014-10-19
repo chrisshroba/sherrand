39682ecda78aded5f6188b5ffeddd113abee055d
Walker Henderson <wjahenderson@gmail.com>
2014-10-19 03:54:50 -0500

    Created a vagrant provisioning script and relevant files for #10

--- src/router.py
+++ src/router.py
@@ -7,12 +7,15 @@ app = Flask(__name__,
             static_folder="../static",
             static_path="")
 
+# Turn off for production
+app.debug = True
+
 
 @app.route('/')
 def root():
     db = MySQLdb.connect(host="localhost",
                          user="root",
-                         passwd="",
+                         passwd="password",
                          db="test")
     cur = db.cursor()
     cur.execute("SELECT * FROM sayings")
@@ -22,4 +25,4 @@ def root():
 
 
 
-app.run(port=80,host="0.0.0.0",debug =True) #We'll turn off debug in production
+#app.run(debug =True) #We'll turn off debug in production
