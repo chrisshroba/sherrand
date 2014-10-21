54b378af330428019af5dff1a7e3b6925e84c495
Serin Yoon <syoon17@illinois.edu>
2014-10-21 18:10:26 -0500

    reorganized and templatized for #4 and #11

--- src/router.py
+++ src/router.py
@@ -23,6 +23,13 @@ def root():
     db.close()
     return sayings[randint(0, len(sayings) - 1)]
 
+@app.route('/request')
+def request_ride():
+    return render_template('request_ride.html')
 
+
+@app.route('/offer')
+def offer_ride():
+    return render_template('offer_ride.html')
 
 #app.run(debug =True) #We'll turn off debug in production
