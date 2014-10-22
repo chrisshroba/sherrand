8acea4f5d1bbe329dd04c1636c3233a4aa4d871f
Serin Yoon <syoon17@illinois.edu>
2014-10-21 22:43:43 -0500

    reorganized and templatized for #4 and #11

--- static/js/script.js
+++ static/js/script.js
@@ -3,9 +3,14 @@ window.onload = function () {
   var rideDate = document.getElementById('ride-date');
   var rideStartTime = document.getElementById('ride-start-time');
   var rideEndTime = document.getElementById('ride-end-time');
-  console.log(currentDate.getFullYear() + "-" + (currentDate.getMonth()+1) + "-" + currentDate.getDate());
-  //rideDate.setAttribute("value", currentDate.getFullYear() + "-" + (currentDate.getMonth()+1) + "-" + currentDate.getDate());
   rideDate.value = currentDate.getFullYear() + "-" + (currentDate.getMonth()+1) + "-" + currentDate.getDate();
   rideStartTime.value = currentDate.getHours() + ":" + currentDate.getMinutes();
   rideEndTime.value = (currentDate.getHours()+1) + ":" + currentDate.getMinutes();
 }
+
+
+// $(function() {
+// 	var currentDate = new Date();
+// 	console.log("here");
+// // 	$('ride-date').val = currentDate.getFullYear() + "-" + (currentDate.getMonth()+1) + "-" + currentDate.getDate();
+// });
