4222a4372aa69e5783a60533f56cd470470544c0
Serin Yoon <syoon17@illinois.edu>
2014-10-21 23:30:44 -0500

    jquery not working for #4 and 1

--- static/js/script.js
+++ static/js/script.js
@@ -1,12 +1,12 @@
-window.onload = function () {
-  var currentDate = new Date();
-  var rideDate = document.getElementById('ride-date');
-  var rideStartTime = document.getElementById('ride-start-time');
-  var rideEndTime = document.getElementById('ride-end-time');
-  rideDate.value = currentDate.getFullYear() + "-" + (currentDate.getMonth()+1) + "-" + currentDate.getDate();
-  rideStartTime.value = currentDate.getHours() + ":" + currentDate.getMinutes();
-  rideEndTime.value = (currentDate.getHours()+1) + ":" + currentDate.getMinutes();
-}
+// window.onload = function () {
+//   var currentDate = new Date();
+//   var rideDate = document.getElementById('ride-date');
+//   var rideStartTime = document.getElementById('ride-start-time');
+//   var rideEndTime = document.getElementById('ride-end-time');
+//   rideDate.value = currentDate.getFullYear() + "-" + (currentDate.getMonth()+1) + "-" + currentDate.getDate();
+//   rideStartTime.value = currentDate.getHours() + ":" + currentDate.getMinutes();
+//   rideEndTime.value = (currentDate.getHours()+1) + ":" + currentDate.getMinutes();
+// }
 
 
 // $(function() {
