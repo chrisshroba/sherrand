6e7d972dbdec8a5998923991bc522157702d8ddb
Christopher Shroba <chrisshroba@gmail.com>
2014-10-22 09:31:25 -0500

    implemented login

--- vagrant/setup.sql
+++ vagrant/setup.sql
@@ -12,3 +12,18 @@ INSERT INTO sayings
 VALUES ("Hello, butter!");
 INSERT INTO sayings
 VALUES ("Hello, system!");
+
+
+CREATE TABLE `user` (
+      `username` varchar(30) DEFAULT NULL,
+      `pass` varchar(160) DEFAULT NULL,
+      `user_id` int(11) NOT NULL AUTO_INCREMENT,
+      `full_name` varchar(60) DEFAULT NULL,
+      `phone` varchar(16) DEFAULT NULL,
+      `email` varchar(40) DEFAULT NULL,
+      `score` int(11) DEFAULT NULL,
+      `photo` varchar(40) DEFAULT NULL,
+      `driver_rating` double DEFAULT NULL,
+      `passenger_rating` double DEFAULT NULL,
+      PRIMARY KEY (`user_id`)
+);
